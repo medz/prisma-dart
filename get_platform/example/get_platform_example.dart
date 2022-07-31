ffe911557d913c948b12eb89d7d6cf3a0a191c5b
Seven Du <whine.whitish.0u@icloud.com>
2022-07-31 15:15:36 +0800

    chore: create get_platform package

---
+++ get_platform/example/get_platform_example.dart
@@ -0,0 +1,6 @@
+import 'package:get_platform/get_platform.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ get_platform/lib/get_platform.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library get_platform;
+
+
+
+// TODO: Export any libraries intended for clients of this package.
