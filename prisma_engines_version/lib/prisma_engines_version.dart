50e174b68d5ccb01ea879044a168fcf27dd067f4
Seven Du <whine.whitish.0u@icloud.com>
2022-07-31 14:30:27 +0800

    feat: add prisma_engines_version package

---
+++ prisma_engines_version/lib/prisma_engines_version.dart
@@ -0,0 +1,6 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library prisma_engines_version;
+
+const String enginesVersion = r'ae7c377d84048fc7fc2e1a1f97780b1f2f3c41dd';
