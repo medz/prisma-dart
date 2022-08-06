d05caf5b9892a3dc084bf991a09f6fe2d51985a7
Seven Du <whine.whitish.0u@icloud.com>
2022-08-06 15:55:20 +0800

    chore(dmmf): add prisma dmmf package

--- prisma_query_engine/lib/src/shared/prisma_query_engine.dart
+++ prisma_query_engine/lib/src/shared/prisma_query_engine.dart
@@ -1,1 +1,5 @@
-abstract class PrismaQueryEngine {}
+/// Fork see: https://github.com/prisma/prisma/blob/main/packages/engine-core/src/common/Engine.ts
+abstract class PrismaQueryEngine {
+  Future<void> start();
+  Future<void> stop();
+}
