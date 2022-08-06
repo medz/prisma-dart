ff1251eb69c20beea9916df4121e26f1598c8fcf
Seven Du <whine.whitish.0u@icloud.com>
2022-08-06 12:06:45 +0800

    chore: WIP

---
+++ prisma_query_engine/README.md
@@ -0,0 +1,8 @@
+Prisma ORM engine wrapper.
+
+## Features
+
+- Prisma binary query engine
+- Prisma query engine for dylib
+- Prisma Data Proxy
+
---
+++ prisma_query_engine/lib/prisma_query_engine.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library prisma_query_engine;
+
+export 'src/prisma_query_engine_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
