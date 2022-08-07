401d3df0b34625860a18e588d1c191c0e9c3596b
Ali <aliammarali125@gmail.com>
2022-08-08 00:55:10 +0300

    chore(prisma_cli): add missing config field
    we need engineType to determine which engine
    will be used by the PrismaClient
    we also  need  'output' dir

--- prisma_cli/lib/src/generator/messages/get_config_response.dart
+++ prisma_cli/lib/src/generator/messages/get_config_response.dart
@@ -1,12 +1,14 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:prisma_cli/src/data_source/data_source_provider.dart';
 
 part 'get_config_response.g.dart';
 
 @JsonSerializable(explicitToJson: true, createFactory: true, createToJson: true)
 class GetConfigResponse {
   final List<GeneratorConfig> generators;
+  final List<Datasource> datasources;
 
-  const GetConfigResponse(this.generators);
+  const GetConfigResponse(this.generators, this.datasources);
 
   factory GetConfigResponse.fromJson(Map<String, dynamic> json) =>
       _$GetConfigResponseFromJson(json);
@@ -17,10 +19,17 @@ class GetConfigResponse {
 @JsonSerializable(explicitToJson: true)
 class GeneratorConfig {
   final String name;
-  final GeneratorProvider provider;
+  final Value provider;
+  final Config config;
+  final Value? output;
+  final List<Value> binaryTargets;
+
   const GeneratorConfig({
     required this.name,
     required this.provider,
+    required this.config,
+    required this.output,
+    required this.binaryTargets,
   });
 
   factory GeneratorConfig.fromJson(Map<String, dynamic> json) =>
@@ -30,15 +39,40 @@ class GeneratorConfig {
 }
 
 @JsonSerializable()
-class GeneratorProvider {
-  final String? value;
-
-  const GeneratorProvider({
-    this.value,
-  });
-
-  factory GeneratorProvider.fromJson(Map<String, dynamic> json) =>
-      _$GeneratorProviderFromJson(json);
-
-  Map<String, dynamic> toJson() => _$GeneratorProviderToJson(this);
+class Config {
+  final EngineType? engineType;
+  final List<String>? binaryTargets;
+  final Value? output;
+  Config(
+      {required this.engineType,
+      required this.binaryTargets,
+      required this.output});
+  factory Config.fromJson(Map<String, dynamic> json) => _$ConfigFromJson(json);
+  Map<String, dynamic> toJson() => _$ConfigToJson(this);
 }
+
+@JsonSerializable()
+class Value {
+  final String? fromEnvVar;
+  final String value;
+  Value(this.fromEnvVar, this.value);
+
+  factory Value.fromJson(Map<String, dynamic> json) => _$ValueFromJson(json);
+  Map<String, dynamic> toJson() => _$ValueToJson(this);
+}
+
+@JsonSerializable()
+class Datasource {
+  final String name;
+  final DataSourceProvider? connectorType;
+  final Value url;
+  final Map? config;
+
+  Datasource(this.name, this.connectorType, this.url, this.config);
+
+  factory Datasource.fromJson(Map<String, dynamic> json) =>
+      _$DatasourceFromJson(json);
+  Map<String, dynamic> toJson() => _$DatasourceToJson(this);
+}
+
+enum EngineType { dataproxy, binary, library }
--- prisma_cli/lib/src/generator/messages/get_config_response.g.dart
+++ prisma_cli/lib/src/generator/messages/get_config_response.g.dart
@@ -11,32 +11,91 @@ GetConfigResponse _$GetConfigResponseFromJson(Map<String, dynamic> json) =>
       (json['generators'] as List<dynamic>)
           .map((e) => GeneratorConfig.fromJson(e as Map<String, dynamic>))
           .toList(),
+      (json['datasources'] as List<dynamic>)
+          .map((e) => Datasource.fromJson(e as Map<String, dynamic>))
+          .toList(),
     );
 
 Map<String, dynamic> _$GetConfigResponseToJson(GetConfigResponse instance) =>
     <String, dynamic>{
       'generators': instance.generators.map((e) => e.toJson()).toList(),
+      'datasources': instance.datasources.map((e) => e.toJson()).toList(),
     };
 
 GeneratorConfig _$GeneratorConfigFromJson(Map<String, dynamic> json) =>
     GeneratorConfig(
       name: json['name'] as String,
-      provider:
-          GeneratorProvider.fromJson(json['provider'] as Map<String, dynamic>),
+      provider: Value.fromJson(json['provider'] as Map<String, dynamic>),
+      config: Config.fromJson(json['config'] as Map<String, dynamic>),
+      output: json['output'] == null
+          ? null
+          : Value.fromJson(json['output'] as Map<String, dynamic>),
+      binaryTargets: (json['binaryTargets'] as List<dynamic>)
+          .map((e) => Value.fromJson(e as Map<String, dynamic>))
+          .toList(),
     );
 
 Map<String, dynamic> _$GeneratorConfigToJson(GeneratorConfig instance) =>
     <String, dynamic>{
       'name': instance.name,
       'provider': instance.provider.toJson(),
+      'config': instance.config.toJson(),
+      'output': instance.output?.toJson(),
+      'binaryTargets': instance.binaryTargets.map((e) => e.toJson()).toList(),
     };
 
-GeneratorProvider _$GeneratorProviderFromJson(Map<String, dynamic> json) =>
-    GeneratorProvider(
-      value: json['value'] as String?,
+Config _$ConfigFromJson(Map<String, dynamic> json) => Config(
+      engineType: $enumDecodeNullable(_$EngineTypeEnumMap, json['engineType']),
+      binaryTargets: (json['binaryTargets'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
+      output: json['output'] == null
+          ? null
+          : Value.fromJson(json['output'] as Map<String, dynamic>),
     );
 
-Map<String, dynamic> _$GeneratorProviderToJson(GeneratorProvider instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$ConfigToJson(Config instance) => <String, dynamic>{
+      'engineType': _$EngineTypeEnumMap[instance.engineType],
+      'binaryTargets': instance.binaryTargets,
+      'output': instance.output,
+    };
+
+const _$EngineTypeEnumMap = {
+  EngineType.dataproxy: 'dataproxy',
+  EngineType.binary: 'binary',
+  EngineType.library: 'library',
+};
+
+Value _$ValueFromJson(Map<String, dynamic> json) => Value(
+      json['fromEnvVar'] as String?,
+      json['value'] as String,
+    );
+
+Map<String, dynamic> _$ValueToJson(Value instance) => <String, dynamic>{
+      'fromEnvVar': instance.fromEnvVar,
       'value': instance.value,
     };
+
+Datasource _$DatasourceFromJson(Map<String, dynamic> json) => Datasource(
+      json['name'] as String,
+      $enumDecodeNullable(_$DataSourceProviderEnumMap, json['connectorType']),
+      Value.fromJson(json['url'] as Map<String, dynamic>),
+      json['config'] as Map<String, dynamic>?,
+    );
+
+Map<String, dynamic> _$DatasourceToJson(Datasource instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'connectorType': _$DataSourceProviderEnumMap[instance.connectorType],
+      'url': instance.url,
+      'config': instance.config,
+    };
+
+const _$DataSourceProviderEnumMap = {
+  DataSourceProvider.postgresql: 'postgresql',
+  DataSourceProvider.mysql: 'mysql',
+  DataSourceProvider.sqlite: 'sqlite',
+  DataSourceProvider.sqlserver: 'sqlserver',
+  DataSourceProvider.mongodb: 'mongodb',
+  DataSourceProvider.cockroachdb: 'cockroachdb',
+};
