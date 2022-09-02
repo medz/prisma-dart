eb443410c478ca5d2d4283bcdfd68dce2eb55f25
Seven Du <whine.whitish.0u@icloud.com>
2022-09-02 17:06:19 +0800

    chore(runtime): Add prisma union zero and one factory

--- lib/src/runtime/prisma_union.dart
+++ lib/src/runtime/prisma_union.dart
@@ -1,7 +1,10 @@
 class PrismaUnion<T0, T1> {
-  final T0? zore;
+  final T0? zero;
   final T1? one;
 
-  const PrismaUnion({this.zore, this.one})
-      : assert(zore != null && one != null, 'zore and one must be not null');
+  const PrismaUnion({this.zero, this.one})
+      : assert(zero != null && one != null, 'zore and one must be not null');
+
+  factory PrismaUnion.zero(T0 zero) => PrismaUnion(zero: zero);
+  factory PrismaUnion.one(T1 one) => PrismaUnion(one: one);
 }
