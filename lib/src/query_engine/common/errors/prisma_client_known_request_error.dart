1b6c90988ef93e24e35716173e6b8b2651d78167
Ali <aliammarali125@gmail.com>
2022-09-04 22:59:39 +0300

    chore(runtime) : override toString from error

--- lib/src/query_engine/common/errors/prisma_client_known_request_error.dart
+++ lib/src/query_engine/common/errors/prisma_client_known_request_error.dart
@@ -2,4 +2,9 @@ class PrismaClientKnownRequestError implements Exception {
   final String message;
 
   const PrismaClientKnownRequestError(this.message);
+  
+  @override
+  String toString() {
+    return "PrismaClientKnownRequestError: $message";
+  }
 }
