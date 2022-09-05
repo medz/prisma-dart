13c9bffe4571d78f67f42875b856e8ec7514f59f
Jacob Moura <jacobaraujo7@gmail.com>
2022-09-04 21:46:12 -0300

    fix utf8 and assert

--- lib/src/query_engine/common/errors/prisma_client_known_request_error.dart
+++ lib/src/query_engine/common/errors/prisma_client_known_request_error.dart
@@ -2,7 +2,7 @@ class PrismaClientKnownRequestError implements Exception {
   final String message;
 
   const PrismaClientKnownRequestError(this.message);
-  
+
   @override
   String toString() {
     return "PrismaClientKnownRequestError: $message";
--- lib/src/runtime/prisma_union.dart
+++ lib/src/runtime/prisma_union.dart
@@ -3,7 +3,7 @@ class PrismaUnion<T0, T1> {
   final T1? one;
 
   const PrismaUnion({this.zero, this.one})
-      : assert(zero != null && one != null, 'zore and one must be not null');
+      : assert(zero != null || one != null, 'zore and one must be not null');
 
   factory PrismaUnion.zero(T0 zero) => PrismaUnion(zero: zero);
   factory PrismaUnion.one(T1 one) => PrismaUnion(one: one);
