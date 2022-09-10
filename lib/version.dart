9a7d30a01e389c2078c32aad3ad94b2d02540d27
Seven Du <whine.whitish.0u@icloud.com>
2022-09-10 17:02:49 +0800

    chore: Sync package version of code

--- lib/version.dart
+++ lib/version.dart
@@ -5,4 +5,4 @@ const String binaryVersion = 'bcc34eccb8bd811bcf624288e5e05b7abda86699';
 const String capiVersion = '0.0.1';
 
 /// The Prisma CLI version.
-const String packageVersion = '2.0.0-dev';
+const String packageVersion = '2.1.2';
