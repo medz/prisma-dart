a1dfc78fe87ed46756c5a147bd6aec23d4f594bb
Seven Du <whine.whitish.0u@icloud.com>
2022-09-11 21:21:30 +0800

    chore: Create data proxy example

---
+++ example/data_proxy/bin/data_proxy.dart
@@ -0,0 +1,5 @@
+import 'package:data_proxy/data_proxy.dart' as data_proxy;
+
+void main(List<String> arguments) {
+  print('Hello world: ${data_proxy.calculate()}!');
+}
---
+++ example/data_proxy/test/data_proxy_test.dart
@@ -0,0 +1,8 @@
+import 'package:data_proxy/data_proxy.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
