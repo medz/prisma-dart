3a21cb2e5fb9b3e0cef79c1e42e0fd55941f8b44
Skill Lan <skillLan@outlook.com>
2022-10-01 16:57:44 +0800

    docs: Create basic & get started

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,1 +1,50 @@
-import {} from "vitepress";
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+    title: "Prisma ORM for Dart",
+    description: "Next-generation ORM for Dart & Flutter | PostgreSQL, MySQL, MariaDB, SQL Server, SQLite, MongoDB and CockroachDB",
+
+    lastUpdated: true,
+    cleanUrls: "without-subfolders",
+
+    head: [
+        // Basic
+        ["link", { rel: "icon", type: "image/png", href: "/logo.png" }],
+
+        // Twitter
+        ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+        ['meta', { name: 'twitter:site', content: '@odroeinc' }],
+    ],
+
+    themeConfig: {
+        logo: "/logo.png",
+
+        socialLinks: [
+            { icon: 'twitter', link: 'https://twitter.com/odroeinc' },
+            { icon: 'github', link: 'https://github.com/odroe/prisma-dart' },
+        ],
+
+        editLink: {
+            pattern: "https://github.com/odroe/prisma-dart/edit/main/docs/:path",
+        },
+
+        nav: [
+            { text: "Getting started", link: "/getting-started" },
+            { text: "Guides", link: "/guides" },
+            { text: "Concepts", link: "/concepts" },
+            {
+                text: "Reference",
+                items: [
+                    { text: "Prisma Schema reference", link: "https://prisma.io/docs/reference/api-reference/prisma-schema-reference" },
+                    { text: "Library API reference", link: "https://pub.dev/documentation/orm/latest/" },
+                    { text: "Command reference", link: "reference/command" },
+                ],
+            },
+            { text: "♥︎ Sponsor", link: "https://opencollective.com/prisma-dart" },
+        ],
+    },
+
+    markdown: {
+        lineNumbers: false,
+    },
+});
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,27 @@
-# Hello Prisma  ORM for Dart
+---
+layout: home
+titleTemplate: false
+hero:
+    name: Prisma ORM
+    text: Next-generation Dart and Flutter ORM
+    tagline: Prisma unlocks a new level of developer experience when working with databases thanks to its intuitive data model, automated migrations, type-safety & auto-completion.
+    image:
+        src: /logo.png
+    actions:
+        - theme: brand
+          text: ⚡️ Get Started
+          link: /getting-started
+        - theme: alt
+          text: ♥︎ Sponsor
+          link: https://opencollective.com/prisma-dart
+        - theme: alt
+          text: View on GitHub
+          link: https://github.com/odroe/prisma-dart
+features:
+    - title: Human Readability
+      details: The Prisma schema is intuitive and lets you declare your database tables in a human-readable way — making your data modeling experience a delight. You define your models by hand or introspect them from an existing database.
+    - title: Type-safe Client
+      details: Prisma Client is a query builder that’s tailored to your schema. We designed its API to be intuitive, both for SQL veterans and developers brand new to databases. The auto-completion helps you figure out your query without the need for documentation.
+    - title: Most Popular Databases
+      details: Prisma works seamlessly across most popular databases and service providers. | PostgreSQL, MySQL, MariaDB, SQL Server, SQLite, MongoDB and CockroachDB.
+---
