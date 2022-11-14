1eee74c0eeeb49b4ad09a59087636a9701682d91
Seven Du <whine.whitish.0u@icloud.com>
2022-11-14 11:23:53 +0800

    chore(engines): Update query engine version

--- lib/version.dart
+++ lib/version.dart
@@ -1,7 +1,7 @@
 library prisma.version;
 
 /// Prisma binary engines version.
-const String binaryVersion = 'c875e43600dfe042452e0b868f7a48b817b9640b';
+const String binaryVersion = '694eea289a8462c80264df36757e4fdc129b1b32';
 
 /// Prisma query engine C API version.
 const String capiVersion = '0.0.1';
@@ -10,4 +10,4 @@ const String capiVersion = '0.0.1';
 const String packageVersion = '2.4.3';
 
 /// Prisma data proxy remote client version.
-const String dataProxyRemoteClientVersion = '4.3.1';
+const String dataProxyRemoteClientVersion = '4.6.1';
