fc55bdb21c52a98048c93f7494dcd65c203780d6
Seven Du <whine.whitish.0u@icloud.com>
2023-01-15 20:37:12 +0800

    chore(example): WIP

--- example/flutter_data_proxy/lib/prisma_configurator.dart
+++ example/flutter_data_proxy/lib/prisma_configurator.dart
@@ -1,8 +1,9 @@
 import 'package:orm/configure.dart';
 
 /// Configure Prisma for production environment.
-/// 
+///
 /// **NOTE**: The function name must be configurePrisma.
 void configurePrisma(PrismaEnvironment environment) {
-  environment['DATABASE_URL'] = r'postgres://postgres:postgres@localhost:5432/postgres?schema=public';
+  environment['DATABASE_URL'] =
+      r'prisma://aws-us-east-1.prisma-data.com/?api_key=<You api key>';
 }
--- example/flutter_data_proxy/prisma/development.dart
+++ example/flutter_data_proxy/prisma/development.dart
@@ -2,7 +2,9 @@ import 'package:orm/configure.dart';
 
 /// Prisma development environment configurator.
 void configurator(PrismaDevelopment development) {
-  development.override('DATABASE_URL', r'postgres://postgres:postgres@localhost:5432/postgres?schema=public');
+  development.override('DATABASE_URL',
+      r'postgres://<User-Password>@ancient-bush-179952.us-east-2.aws.neon.tech/neondb');
+  development.override('PRISMA_GENERATE_DATAPROXY', 'true');
 
   // You can override environment variable example:
   // development.override('DEBUG', 'true');
@@ -12,8 +14,8 @@ void configurator(PrismaDevelopment development) {
 }
 
 /// Configure Prisma for development environment.
-/// 
+///
 /// **NOTE**: Prisma development must is a executable.
-/// 
+///
 /// The `main` function is a Dart executable file entrypoint.
 void main() => PrismaDevelopment.server(configurator);
