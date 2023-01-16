23c52ea7a6fe3ce2d3e8ed881bf0a53f953d5745
Seven Du <whine.whitish.0u@icloud.com>
2023-01-16 14:58:43 +0800

    chore(binary engine): WIP

---
+++ packages/prisma_engine_binary/example/prisma_engine_binary_example.dart
@@ -0,0 +1,6 @@
+import 'package:prisma_engine_binary/prisma_engine_binary.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/prisma_engine_binary/lib/prisma_engine_binary.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library prisma_engine_binary;
+
+export 'src/engine.dart';
+
+// TODO: Export any libraries intended for clients of this package.
