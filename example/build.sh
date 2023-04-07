70ebe874f0b386393730f6722ceb2e13309aed2d
Seven Du <whine.whitish.0u@icloud.com>
2023-04-08 06:45:28 +0800

    chore(example): add docker example

--- example/build.sh
+++ example/build.sh
@@ -1,10 +1,1 @@
-# Build the example project docker image
-
-## image name
-IMAGE_NAME=example
-
-## image version
-IMAGE_VERSION=1.0.0
-
-## build image
-docker build -t ${IMAGE_NAME}:${IMAGE_VERSION} .
+docker build --platform=amd64 -t exanple .
