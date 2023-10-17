94b9883f5c5fe37227e0409f8608237545785d91
Seven Du <whine.whitish.0u@icloud.com>
2023-10-17 23:56:00 +0800

    chore: WIP

---
+++ lib/generator_helper.dart
@@ -0,0 +1,4 @@
+library prisma.generator_helper;
+
+export 'src/generator_helper/create_generator.dart';
+export 'src/generator_helper/types.dart';
---
+++ lib/src/dmmf.dart
@@ -0,0 +1,1 @@
+enum FieldKind { scalar, object, enum, unsupported }
