9d6ebcfea1de0bf27cd9155a82b176a19404e2a0
Seven Du <whine.whitish.0u@icloud.com>
2023-11-01 01:21:23 +0800

    chore: WIP

--- src/dmmf.rs
+++ src/dmmf.rs
@@ -1,12 +1,12 @@
 use libc::c_char;
-use prisma_dmmf::dmmf_json_from_schema;
+use prisma_request_handlers::dmmf;
 
 use crate::utils;
 
 #[no_mangle]
 pub extern  "C" fn parse(schema: *const c_char) -> *const c_char {
     let schema = utils::str::form(schema);
-    let dmmf = dmmf_json_from_schema(schema);
+    let schema = prisma_psl::validate(schema.into());
 
-    utils::str::to_str(dmmf)
+    schema.db;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 mod utils;
+mod error;
 
 pub mod dmmf;
