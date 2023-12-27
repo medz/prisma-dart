c327cfb47cfb77dd0b7966665b5619fc1886d52c
Seven Du <whine.whitish.0u@icloud.com>
2023-12-27 15:44:57 +0800

    chore: WIP

--- example/main.dart
+++ example/main.dart
@@ -1,34 +1,34 @@
-import 'package:orm/engines/binary.dart';
-import 'package:orm/orm.dart';
+// import 'package:orm/engines/binary.dart';
+// import 'package:orm/orm.dart';
 
-import '../prisma/dart/client.dart';
-import '../prisma/dart/dmmf.dart';
+// import '../prisma/dart/client.dart';
+// import '../prisma/dart/dmmf.dart';
 
-void main() async {
-  final engine = BinaryEngine(
-    datasource: 'db',
-    schema: schema,
-    binary: 'prisma/dart/prisma-query-engine',
-    url: Uri.parse('postgresql://seven@localhost:5432/prisma-dart'),
-  );
-  final prisma = PrismaClient(engine: engine);
-  await prisma.$connect();
+// void main() async {
+//   final engine = BinaryEngine(
+//     datasource: 'db',
+//     schema: schema,
+//     binary: 'prisma/dart/prisma-query-engine',
+//     url: Uri.parse('postgresql://seven@localhost:5432/prisma-dart'),
+//   );
+//   final prisma = PrismaClient(engine: engine);
+//   await prisma.$connect();
 
-  final query = serializeJsonQuery(
-    datamodel: dmmf.datamodel,
-    action: JsonQueryAction.aggregate,
-    modelName: 'Post',
-    args: {
-      'select': {
-        '_count': {
-          "select": {'_all': true}
-        },
-      },
-    },
-  );
+//   final query = serializeJsonQuery(
+//     datamodel: dmmf.datamodel,
+//     action: JsonQueryAction.aggregate,
+//     modelName: 'Post',
+//     args: {
+//       'select': {
+//         '_count': {
+//           "select": {'_all': true}
+//         },
+//       },
+//     },
+//   );
 
-  final result = await engine.request(query, action: 'aggregatePost');
-  print(result);
+//   final result = await engine.request(query, action: 'aggregatePost');
+//   print(result);
 
-  await prisma.$disconnect();
-}
+//   await prisma.$disconnect();
+// }
