289ab3e0c3d600f2608609c89d6afa512481813f
Seven Du <whine.whitish.0u@icloud.com>
2024-01-01 20:09:45 +0800

    chore: WIP

---
+++ bin/orm.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+import 'package:orm/generator_helper.dart';
+
+void main() async {
+  final app = Generator.stdio(stdin: stdin, stdout: stderr);
+}
--- lib/orm.dart
+++ lib/orm.dart
@@ -10,28 +10,6 @@ export 'src/runtime/prisma_null.dart';
 export 'src/runtime/reference.dart';
 export 'src/runtime/transaction.dart';
 
-// Action
-export 'src/runtime/action/action.dart';
-export 'src/runtime/action/action_options.dart';
-export 'src/runtime/action/action+cursor.dart';
-export 'src/runtime/action/action+data.dart';
-export 'src/runtime/action/action+distinct.dart';
-export 'src/runtime/action/action+from.dart';
-export 'src/runtime/action/action+having.dart';
-export 'src/runtime/action/action+order_by.dart';
-export 'src/runtime/action/action+pagination.dart';
-export 'src/runtime/action/action+upsert.dart';
-export 'src/runtime/action/action+where.dart';
-
-// Input
-export 'src/runtime/input/input.dart';
-export 'src/runtime/input/input_builder.dart';
-export 'src/runtime/input/input_builder+call.dart';
-export 'src/runtime/input/input_builder+nulls.dart';
-export 'src/runtime/input/input_builder+ref.dart';
-export 'src/runtime/input/order_by.dart';
-export 'src/runtime/input/filters/num_filter.dart';
-
 // JSON Protocol
 export 'src/runtime/json_protocol/protocol.dart';
 export 'src/runtime/json_protocol/serialize.dart';
