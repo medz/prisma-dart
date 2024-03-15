6a0133721a12a76e700f8e389877108d557246e8
Seven Du <whine.whitish.0u@icloud.com>
2024-03-15 21:18:34 +0800

    chore: remove issue 340 example

--- i340/bin/i340.dart
+++
@@ -1,5 +0,0 @@
-import 'package:i340/i340.dart' as i340;
-
-void main(List<String> arguments) {
-  print('Hello world: ${i340.calculate()}!');
-}
--- i340/test/i340_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:i340/i340.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
