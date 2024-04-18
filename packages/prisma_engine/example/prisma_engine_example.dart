9bca5157c5bace9c2b8b30000f989652fd747e20
Seven Du <whine.whitish.0u@icloud.com>
2024-04-18 18:07:54 +0800

    chore: WIP

--- packages/prisma_engine/example/prisma_engine_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:prisma_engine/prisma_engine.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- packages/prisma_engine/lib/prisma_engine.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
-
-export 'src/prisma_engine_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
