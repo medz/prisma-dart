e501651fac88b46ccd8ff1fea61bd9765e11f0ba
Seven Du <whine.whitish.0u@icloud.com>
2024-05-27 13:57:10 +0800

    chore: WIP

--- packages/prisma_flutter2/lib/prisma_flutter.dart
+++
@@ -1,1 +0,0 @@
-export 'src/library_engine.dart';
--- packages/prisma_flutter2/src/prisma_flutter.c
+++
@@ -1,5 +0,0 @@
-#include "prisma_flutter.h"
-
-FFI_PLUGIN_EXPORT int initialize_prisma_query_engine(struct ConstructorOptions options, struct QueryEngine **qe_ptr, char **error_string_ptr) {
-    return prisma_create(options, qe_ptr, error_string_ptr);
-}
