957372a762c2ecd2806d72945978fa94883153b7
Seven Du <whine.whitish.0u@icloud.com>
2024-06-05 15:32:25 +0800

    refactor(client): add base client and client options

---
+++ packages/orm/lib/src/base_prisma_client.dart
@@ -0,0 +1,26 @@
+import 'logging.dart';
+import 'prisma_client_options.dart';
+import 'runtime/engine.dart';
+
+/// Base prisma client
+abstract class BasePrismaClient<E extends Engine> {
+  BasePrismaClient({
+    String? datasourceUrl,
+    final Map<String, String>? datasources,
+    ErrorFormat errorFormat = ErrorFormat.colorless,
+    LogDefinition? log,
+  }) : _options = PrismaClientOptions(
+          datasources: datasources,
+          datasourceUrl: datasourceUrl,
+          errorFormat: errorFormat,
+          logEmitter: LogEmitter(log ?? const {}),
+        );
+
+  final PrismaClientOptions _options;
+
+  /// Prisma client options
+  PrismaClientOptions get $options => _options;
+
+  /// Returns the [Engine] instance typeof [E].
+  E get $engine;
+}
---
+++ packages/orm/lib/src/prisma_client_options.dart
@@ -0,0 +1,43 @@
+import 'logging.dart';
+
+/// Error format
+enum ErrorFormat {
+  ///Enables pretty error formatting.
+  pretty,
+
+  ///Enables colorless error formatting.
+  colorless,
+
+  /// Enables minimal error formatting.
+  minimal
+}
+
+class PrismaClientOptions {
+  /// Overwrites the primary datasource url from your schema.prisma file
+  final String? datasourceUrl;
+
+  /// Overwrites the datasource url from your schema.prisma file
+  ///
+  /// Example:
+  /// ```dart
+  /// final prisma = PrismaClient(
+  ///   datasources: {'db': 'file:./dev.db'}
+  /// );
+  /// ```
+  final Map<String, String>? datasources;
+
+  /// Determines the level and formatting of errors returned by Prisma Client.
+  final ErrorFormat errorFormat;
+
+  /// Log emitter.
+  final LogEmitter logEmitter;
+
+  /// Create Prisma client options
+  const PrismaClientOptions({
+    this.datasourceUrl,
+    this.datasources,
+    required this.errorFormat,
+    required this.logEmitter,
+  }) : assert(!(datasourceUrl != null && datasources != null),
+            'DatasourceUrl and datasources cannot be used together');
+}
