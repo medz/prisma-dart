445d1969223017656b47f1ee4eb8ef2d64f7a3b9
Seven Du <shiweidu@outlook.com>
2024-11-03 20:50:12 +0800

    chore: done android

--- packages/orm_flutter_ffi/lib/src/bindings.dart
+++ packages/orm_flutter_ffi/lib/src/bindings.dart
@@ -6,6 +6,8 @@ import '_generate_bindings.dart';
 DynamicLibrary get _dl {
   if (Platform.isIOS) {
     return DynamicLibrary.open('orm_flutter_ios.framework/orm_flutter_ios');
+  } else if (Platform.isAndroid) {
+    return DynamicLibrary.open("liborm_flutter_android.so");
   }
 
   throw UnsupportedError('Susupported platform: ${Platform.operatingSystem}');
